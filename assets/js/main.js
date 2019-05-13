33de6be04e0195a8685a52931ba13688cce663fc
Javier Gonzalez Llamas <lastoner4@gmail.com>
2019-05-13 17:59:58 +0000

    Changed values and added control with letter w a s e

--- assets/js/main.js
+++ assets/js/main.js
@@ -6,17 +6,18 @@ function moveBlock(e) {
 
   var character = $("#character-div");
   var position = character.position();
+  console.log(character.position());
 
-  if (e.keyCode == '38' && position.top != 0) {
+  if (position.top > 10 && e.keyCode == '38' || e.keyCode == '87' && position.top > 10) {
     character.css("top", position.top - 10);
   }
-  else if (e.keyCode == '40' && position.top < $(window).height() - 45) {
+  else if (position.top < $(window).height() - 55 && e.keyCode == '40' || e.keyCode == '83' && position.top < $(window).height() - 55) {
     character.css("top", position.top + 10);
   }
-  else if (e.keyCode == '37' && position.left != 0) {
+  else if (position.left > 10 && e.keyCode == '37' || e.keyCode == '65' && position.left > 10) {
     character.css("left", position.left - 10);
   }
-  else if (e.keyCode == '39' && position.left < $(window).width() - 50) {
+  else if (position.left < $(window).width() - 50 && e.keyCode == '39' || e.keyCode == '68' && position.left < $(window).width() - 50) {
     character.css("left", position.left + 10);
   }
 }
